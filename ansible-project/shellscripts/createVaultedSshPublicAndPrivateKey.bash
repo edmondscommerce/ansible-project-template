53c3a452d3c550789b86641c962a1bccc78bc16f
Daniel Callaghan <dan@office>
2019-03-07 10:21:49 +0000

    Updated SSH password generation

--- ansible-project/shellscripts/createVaultedSshPublicAndPrivateKey.bash
+++ ansible-project/shellscripts/createVaultedSshPublicAndPrivateKey.bash
@@ -16,13 +16,35 @@ if (( $# < 1 ))
 then
     echo "
 
-    Usage ./$(basename $0) ssh_key_prefix (email, defaults to info@edmondscommerce.co.uk)
+    Usage ./$(basename $0)ssh_key_prefix (email, defaults to info@edmondscommerce.co.uk)
 
     "
     exit 1
 fi
 
 readonly sshKeyPrefix=$1
+if [[ "$sshKeyPrefix" == "" ]]
+then
+    echo "We need a prefix for this key";
+    exit 1;
+fi
+
+printf "Enter desired SSH key pass phrase:\n";
+read -rs sshKeyPass;
+if [[ "$sshKeyPass" == "" ]]
+then
+    echo "We need a password to generate the SSH keys";
+    exit 1;
+fi
+
+printf "Confirm passphrase:\n"
+read -rs sshKeyPassConfirm;
+if [[ "$sshKeyPass" != "$sshKeyPassConfirm" ]]
+then
+    echo "Passwords do not match, please re run the script";
+    exit 1;
+fi
+
 readonly sshKeyEmail=${2:-"info@edmondscommerce.co.uk"}
 readonly sshPrivateKeyPath=~/.ssh/${sshKeyPrefix}_rsa
 readonly sshPublicKeyPath=~/.ssh/${sshKeyPrefix}_rsa.pub
@@ -42,9 +64,7 @@ then
     exit 1
 fi
 
-usersPassword="$(cd $DIR/../ && ansible joseph -i environment/prod -m debug -a 'var=users_password' | grep 'users_password' | cut -d ':' -f 2 | sed -e 's/^ "//' -e 's/"$//')"
-
-ssh-keygen -t rsa -b 4096 -o -a 100 -f ${sshPrivateKeyPath} -q -N "$usersPassword" -C "$sshKeyEmail"
+ssh-keygen -t rsa -b 4096 -o -a 100 -f ${sshPrivateKeyPath} -q -N "$sshKeyPass" -C "$sshKeyEmail"
 
 ./createVaultedDataFromFile.bash $sshPrivateKeyPath ${sshKeyPrefix}_private_key
 
