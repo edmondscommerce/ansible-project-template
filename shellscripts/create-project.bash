a86204cc2623554f36b5b9dea2c5380bbc89333e
dan <dan@edmondscommerce.co.uk>
2020-04-08 10:48:17 +0100

    Updated script headers, added update script for existing projects

--- shellscripts/create-project.bash
+++ shellscripts/create-project.bash
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
+set -e
+set -u
+set -o pipefail
+standardIFS="$IFS"
+IFS=$'\n\t'
+echo "
+===========================================
+$(hostname) $0 $@
+===========================================
+"
 
 usage()
 {
@@ -72,3 +84,9 @@ echo "Installing hooks";
 (cd ${projectPath}; pre-commit install);
 
 echo "Done! New project is available here: ${projectPath}";
+
+echo "
+----------------
+$(hostname) $0 completed
+----------------
+"
---
+++ shellscripts/update.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
+set -e
+set -u
+set -o pipefail
+standardIFS="$IFS"
+IFS=$'\n\t'
+echo "
+===========================================
+$(hostname) $0 $@
+===========================================
+"
+
+echo "Ensuring commit hooks are installed";
+cd ../;
+pre-commit install;
+
+
+echo "
+----------------
+$(hostname) $0 completed
+----------------
+"
